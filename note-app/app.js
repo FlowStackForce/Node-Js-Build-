bf224e1b0e19d8fb3f2f73f811126df73bad2e70
folarin23 <adedejifolarin@yahoo.com>
2021-05-03 10:58:16 +0100

    Notes App in Node

---
+++ note-app/app.js
@@ -0,0 +1,12 @@
+const validator = require('validator')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+const chalk = require('chalk')
+
+const msg = getNotes()
+console.log(msg)
+
+console.log(validator.isEmail('kan.adedeji@gmail.com'))
+console.log(chalk.green('Success'))
+console.log(process.argv)
+console.log(yargs.argv)
